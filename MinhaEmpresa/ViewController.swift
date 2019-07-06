738aebe40a4f527e4cbb117a16c56c8761b88961
Chrystian Salgado <salgado.dev@gmail.com>
2019-07-06 16:38:44 -0300

    Initial Commit

---
+++ MinhaEmpresa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MinhaEmpresa
+//
+//  Created by Chrystian Salgado on 06/07/19.
+//  Copyright © 2019 Salgado's Production. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
