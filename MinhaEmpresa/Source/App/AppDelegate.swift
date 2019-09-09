14a36994df20151d143a9971a7af1f00db0d9ad3
Chrystian Salgado <salgado.dev@gmail.com>
2019-09-09 10:29:52 -0300

    change status bar colot to light

--- MinhaEmpresa/Source/App/AppDelegate.swift
+++ MinhaEmpresa/Source/App/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        application.statusBarStyle = .lightContent
+        
         let window = UIWindow(frame: UIScreen.main.bounds)
         let appCoordinator = AppCoordinator(window: window)
         
