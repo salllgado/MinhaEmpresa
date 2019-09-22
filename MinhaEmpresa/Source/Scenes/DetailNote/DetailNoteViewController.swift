51ac95959e255426ed94f71ff8f44270f9a7ab60
Chrystian Salgado <salgado.dev@gmail.com>
2019-09-22 11:31:56 -0300

    fix: style to ipad

--- MinhaEmpresa/Source/Scenes/DetailNote/DetailNoteViewController.swift
+++ MinhaEmpresa/Source/Scenes/DetailNote/DetailNoteViewController.swift
@@ -25,7 +25,7 @@ class DetailNoteViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        title = "Detalhes"
         setupUI()
     }
     
